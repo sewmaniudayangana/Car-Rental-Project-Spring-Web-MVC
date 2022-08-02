4534db01bb87be17f5b34d661a9239c53bf5e7c8
sewmanudayangana <sewmaniudayangana@gmail.com>
2022-08-02 14:34:55 +0530

    Update

--- src/main/java/lk/ijse/spring/service/impl/CarServiceImpl.java
+++ src/main/java/lk/ijse/spring/service/impl/CarServiceImpl.java
@@ -1,4 +1,63 @@
 package lk.ijse.spring.service.impl;
 
-public class CarServiceImpl {
+import lk.ijse.spring.dto.CarDto;
+import lk.ijse.spring.entity.Car;
+import lk.ijse.spring.repo.CarRepo;
+import lk.ijse.spring.service.CarService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CarServiceImpl implements CarService {
+
+    @Autowired
+    private CarRepo repo;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Override
+    public void VehicleSave(CarDto dto) {
+        if(!repo.existsById(dto.getCid())) {
+            repo.save(mapper.map(dto, Car.class));
+        } else {
+            throw new RuntimeException("Car Already Exist...!");
+        }
+
+    }
+
+    @Override
+    public void DeleteVehicle(String id) {
+        if (repo.existsById(id)) {
+            repo.deleteById(id);
+        } else {
+            throw new RuntimeException("Please check the Car Id.., No Such Car...!");
+        }
+    }
+
+    @Override
+    public void updateVehicle(CarDto dto) {
+        if (repo.existsById(dto.getCid())) {
+            repo.save(mapper.map(dto, Car.class));
+        }
+    }
+
+    @Override
+    public CarDto searchVehicle(String id) {
+        if (repo.existsById(id)) {
+            return mapper.map(repo.findById(id).get(), CarDto.class);
+
+        } else {
+            throw new RuntimeException("No Car For"+ id +"...!");
+        }
+    }
+
+    @Override
+    public List<CarDto> getAllVehicles() {
+        return mapper.map(repo.findAll(), new TypeToken<List<CarDto>>(){
+
+        }.getType());
+    }
 }
